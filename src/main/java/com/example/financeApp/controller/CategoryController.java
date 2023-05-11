616b791c125730090b5eca97ea5d65830c084cf1
Diana Nekrasova <ya.nel2014@ya.ru>
2023-05-11 15:33:45 +0300

    some changes

--- src/main/java/com/example/financeApp/controller/CategoryController.java
+++ src/main/java/com/example/financeApp/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.example.financeApp.controller;
 import com.example.financeApp.dto.CategoryDTO;
 import com.example.financeApp.entity.Category;
 import com.example.financeApp.service.CategoryService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/categories")
 @AllArgsConstructor
+@Tag(name = "categories", description = "Transaction categories")
 public class CategoryController {
     private final CategoryService categoryService;
     @PostMapping
