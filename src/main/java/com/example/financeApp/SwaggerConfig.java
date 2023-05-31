9e38be5956918e3bf8b27f0531b4399af932d96d
Diana Nekrasova <ya.nel2014@ya.ru>
2023-05-31 19:10:25 +0300

    delete comments

--- src/main/java/com/example/financeApp/SwaggerConfig.java
+++ src/main/java/com/example/financeApp/SwaggerConfig.java
@@ -1,38 +1,5 @@
 package com.example.financeApp;
 
-
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//
-//@Configuration
-//@EnableSwagger2
-//@EnableWebMvc
-//class SwaggerConfig {
-//    @Bean
-//    public Docket api( ){
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("com.example.app"))
-//                .paths(PathSelectors.any())
-//                .build();
-//    }
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("swagger-ui.html")
-//                .addResourceLocations("classpath:/META-INF/resources/");
-//
-//        registry.addResourceHandler("/webjars/**")
-//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-//    }
-//}
-
-
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
